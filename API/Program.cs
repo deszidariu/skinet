d57b869f2547abc4b05da719f0dcc18c8d37f063
Deszi <dariu.deszi@ifm.com>
2026-05-23 15:11:00 +0300

    add dbcontext and Products as DbSet

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,14 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container. here we add services to the dependency injection container.
+
+builder.Services.AddDbContext<StoreContext>(options => 
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 builder.Services.AddControllers();
 // builder.Services.AddEndpointsApiExplorer();
