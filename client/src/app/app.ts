6cf7c62adff09a8bb64ba253f6ed8aa5a60c7e21
Deszi <dariu.deszi@ifm.com>
2026-05-23 21:19:22 +0300

    make first http request to server

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,6 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './layout/header/header';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,22 @@ import { Header } from './layout/header/header';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {}
+export class App implements OnInit {
+  baseUrl='http://localhost:5000/api/';
+  
+  //private http = inject(HttpClient); // another way to inject services in components
+  
+  constructor(private http: HttpClient) {
+
+  }  
+
+  ngOnInit(): void {
+    // Initialization logic here
+
+    this.http.get(this.baseUrl + 'products').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error),
+      complete: () => console.log('Request completed')
+    });
+  }
+}
