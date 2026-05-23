bb0c2427cfc22edf73fac2715546f734780f328e
Deszi <dariu.deszi@ifm.com>
2026-05-23 14:34:06 +0300

    set up the API program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -1,24 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container. here we add services to the dependency injection container.
 
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+var app = builder.Build(); // everything that happens before this line is considered services configuration, and everything that happens after this line is considered middleware configuration.
+// Configure the HTTP request pipeline. A http request is going through the middleware, before will reach the controller, and when goes back from the controller will go again through the middleware.
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//app.UseHttpsRedirection();
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+//app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+app.Run();// this is executed at dotnet run.
