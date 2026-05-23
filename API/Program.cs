5b5fbab3405564631d538a2253094a706faf4103
Deszi <dariu.deszi@ifm.com>
2026-05-23 21:19:01 +0300

    add cors to allow angular app to call clinet

--- API/Program.cs
+++ API/Program.cs
@@ -14,6 +14,8 @@ builder.Services.AddControllers();
 // builder.Services.AddEndpointsApiExplorer();
 // builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors();
+
 var app = builder.Build(); // everything that happens before this line is considered services configuration, and everything that happens after this line is considered middleware configuration.
 // Configure the HTTP request pipeline. A http request is going through the middleware, before will reach the controller, and when goes back from the controller will go again through the middleware.
 // if (app.Environment.IsDevelopment())
@@ -23,6 +25,8 @@ var app = builder.Build(); // everything that happens before this line is consid
 // }
 
 //app.UseHttpsRedirection();
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 //app.UseAuthorization();
 
